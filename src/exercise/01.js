ef878fd46cbffdd2323abcab0fa730b1eab92c3f
minjee <minjeeson.dev@gmail.com>
2024-02-14 23:50:10 +0000

    Exercise 1 : useState - greeting

--- src/exercise/01.js
+++ src/exercise/01.js
@@ -1,21 +1,27 @@
 // useState: greeting
 // http://localhost:3000/isolated/exercise/01.js
 
-import * as React from 'react'
+import {useState} from 'react'
 
-function Greeting() {
-  // 💣 delete this variable declaration and replace it with a React.useState call
-  const name = ''
+function Greeting({initialName = ''}) {
+  /*
+    "useState" is one of the basic hooks from react.
+    "useState" : This component can re-render any time this state that I want you to manage is going to change.
+    The purpose of "useState" is to declare a state variable and a function that can be used to update that variable.
+  */
+
+  /* 1. 💯 accept an initialName */
+  const [name, setName] = useState(initialName)
 
   function handleChange(event) {
-    // 🐨 update the name here based on event.target.value
+    setName(event.target.value)
   }
 
   return (
     <div>
       <form>
         <label htmlFor="name">Name: </label>
-        <input onChange={handleChange} id="name" />
+        <input value={name} onChange={handleChange} id="name" />
       </form>
       {name ? <strong>Hello {name}</strong> : 'Please type your name'}
     </div>
